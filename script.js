632ac9015924d3f9f8f0c15201554d494248f4f8
Racheal Wilson R <rachealwilsonr@gmail.com>
2026-02-15 22:22:46 +0530

    script.js

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ function startApp() {
 
 function chooseRole(role) {
   clickSound.play();
+
   document.getElementById("roleSelect").classList.add("hidden");
   document.getElementById("invitation").classList.remove("hidden");
 
+  // 🎵 Role Based Music
   let music = {
     pu: "music/pu.mp3",
     degree: "music/degree.mp3",
@@ -21,6 +23,24 @@ function chooseRole(role) {
   bgm.loop = true;
   bgm.play();
 
+  // 🖼 Load Images Based on Role
+  let gallery = document.getElementById("gallery");
+  gallery.innerHTML = "";
+
+  let images = {
+    pu: ["pu1.jpg", "pu2.jpg", "pu3.jpg"],
+    degree: ["degree1.jpg", "degree2.jpg"],
+    staff: ["staff1.jpg", "staff2.jpg"]
+  };
+
+  images[role].forEach(file => {
+    let img = document.createElement("img");
+    img.src = "images/" + file;
+    img.onclick = () => openFull(img);
+    gallery.appendChild(img);
+  });
+
+  // 📲 QR Code
   new QRCode(document.getElementById("qrcode"), {
     text: window.location.href,
     width: 160,
